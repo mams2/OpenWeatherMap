ee5cb9f7cc99fd107244b9f1c3ae09ec9b29d023
Marcilio <mams2@cin.ufpe.br>
2016-12-24 15:27:41 -0300

    faz a lista das cidades

--- app/src/main/java/com/example/root/openweathermap/ListCities.java
+++ app/src/main/java/com/example/root/openweathermap/ListCities.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -45,6 +49,7 @@ public class ListCities extends AppCompatActivity {
     public class GetHttp extends AsyncTask<String, Void, RespostaServidor> {
         private Exception exception;
         private ProgressDialog load;
+        private ListView lista;
 
         @Override
         protected void onPreExecute(){
@@ -70,6 +75,34 @@ public class ListCities extends AppCompatActivity {
                 System.out.println(resposta.cities.get(0).weather.get(0).description);
                 System.out.println(resposta.cities.get(0).temperature.temp_min);
                 System.out.println(resposta.cities.get(0).temperature.temp_max);
+                String[] result = new String[15];
+                for(int i=0; i<15; i++) result[i] = resposta.cities.get(i).name;
+                ArrayAdapter<String> adapter =
+                        new ArrayAdapter<String>(getBaseContext(),
+                                android.R.layout.simple_list_item_1, result);
+                System.out.println("passou");
+                lista = (ListView) findViewById(R.id.lista);
+                lista.setAdapter(adapter);
+                // Coloca depois da linha no seu OnCreate onde você pega a referência do ListView
+// seta o listener para pegar o click do usuário no item do seu list View
+                lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        // pega o o item selecionado com os dados da pessoa
+                        String medico = (String) lista.getItemAtPosition(position);
+                        System.out.println(medico);
+
+                        // cria a intent
+//                        Intent intent = new Intent(this, DetalhesMedicoActivity.class);
+                        // seta o parametro do medico a exibir os dados
+//                        intent.putExtra( "MEDICO" , medico );
+                        //  chama a Activity que mostra os detalhes
+//                        startActivity(intent);
+
+                    }
+
+                });
             }
 
             load.dismiss();
